7dbf586730ed870498f1e898bd6f5e2695077005
raowx <18033419212@qq.com>
2016-07-13 17:26:32 +0800

    修改

---
+++ th-service/src/main/java/com/tianhong/constant/MenuConstant.java
@@ -0,0 +1,22 @@
+/**  
+ * @Title: MenuConstant.java
+ * @Package com.tianhong.constant
+ * @Description: 描述
+ * @author xing
+ * @date 2016年7月13日 下午5:12:27
+ */
+package com.tianhong.constant;
+
+/**
+ * ClassName: MenuConstant
+ * 
+ * @Description: 描述
+ * @author xing
+ * @date 2016年7月13日 下午5:12:27
+ */
+public class MenuConstant {
+
+	public static final byte MENU_LEVEL_1 = 1;
+	public static final byte MENU_LEVEL_2 = 2;
+	public static final byte MENU_LEVEL_3 = 3;
+}
