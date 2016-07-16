54b81f8e6755485560608a5018c326ef306f2eb7
raowx <18033419212@qq.com>
2016-07-16 11:12:38 +0800

    权限表

---
+++ th-service/src/main/java/com/tianhong/dao/sys/SysRoleMapper.java
@@ -0,0 +1,8 @@
+package com.tianhong.dao.sys;
+
+import com.tianhong.dao.base.BaseMapper;
+import com.tianhong.domain.sys.SysRole;
+
+public interface SysRoleMapper extends BaseMapper<SysRole> {
+
+}
---
+++ th-service/src/main/java/com/tianhong/domain/sys/SysRole.java
@@ -0,0 +1,125 @@
+package com.tianhong.domain.sys;
+
+import java.util.Date;
+
+public class SysRole {
+    private Integer id;
+
+    private String code;
+
+    private String roleName;
+
+    private String roleDescription;
+
+    private Date createTime;
+
+    private Integer createId;
+
+    private Date updateTime;
+
+    private Integer updateId;
+
+    private Boolean isDeleted;
+
+    private String backup1;
+
+    private String backup2;
+
+    private String backup3;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName == null ? null : roleName.trim();
+    }
+
+    public String getRoleDescription() {
+        return roleDescription;
+    }
+
+    public void setRoleDescription(String roleDescription) {
+        this.roleDescription = roleDescription == null ? null : roleDescription.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getCreateId() {
+        return createId;
+    }
+
+    public void setCreateId(Integer createId) {
+        this.createId = createId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getUpdateId() {
+        return updateId;
+    }
+
+    public void setUpdateId(Integer updateId) {
+        this.updateId = updateId;
+    }
+
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public String getBackup1() {
+        return backup1;
+    }
+
+    public void setBackup1(String backup1) {
+        this.backup1 = backup1 == null ? null : backup1.trim();
+    }
+
+    public String getBackup2() {
+        return backup2;
+    }
+
+    public void setBackup2(String backup2) {
+        this.backup2 = backup2 == null ? null : backup2.trim();
+    }
+
+    public String getBackup3() {
+        return backup3;
+    }
+
+    public void setBackup3(String backup3) {
+        this.backup3 = backup3 == null ? null : backup3.trim();
+    }
+}
