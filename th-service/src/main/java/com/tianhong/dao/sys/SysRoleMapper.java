a6ab1c22c00ecb5ac2cfb5a292ada57d6ec7a937
raowx <18033419212@qq.com>
2016-07-16 11:10:49 +0800

    冲突问题

--- th-service/src/main/java/com/tianhong/dao/sys/SysRoleMapper.java
+++
@@ -1,8 +0,0 @@
-package com.tianhong.dao.sys;
-
-import com.tianhong.dao.base.BaseMapper;
-import com.tianhong.domain.sys.SysRole;
-
-public interface SysRoleMapper extends BaseMapper<SysRole> {
-
-}
