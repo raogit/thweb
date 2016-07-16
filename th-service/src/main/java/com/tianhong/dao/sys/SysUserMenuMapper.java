7af99f88b7f078796c799cf99ffec0b035f6fe3d
raowx <18033419212@qq.com>
2016-07-16 11:18:56 +0800

    权限表

---
+++ th-service/src/main/java/com/tianhong/dao/sys/SysUserMenuMapper.java
@@ -0,0 +1,8 @@
+package com.tianhong.dao.sys;
+
+import com.tianhong.dao.base.BaseMapper;
+import com.tianhong.domain.sys.SysUserMenu;
+
+public interface SysUserMenuMapper extends BaseMapper<SysUserMenu> {
+
+}
---
+++ th-service/src/main/java/com/tianhong/domain/sys/SysUserMenu.java
@@ -0,0 +1,115 @@
+package com.tianhong.domain.sys;
+
+import java.util.Date;
+
+public class SysUserMenu {
+    private Integer id;
+
+    private Integer menuId;
+
+    private Integer userId;
+
+    private Date createTime;
+
+    private Integer createId;
+
+    private Date updateTime;
+
+    private Integer updateId;
+
+    private Boolean isDeleted;
+
+    private String backup1;
+
+    private String backup2;
+
+    private String backup3;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(Integer menuId) {
+        this.menuId = menuId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getCreateId() {
+        return createId;
+    }
+
+    public void setCreateId(Integer createId) {
+        this.createId = createId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getUpdateId() {
+        return updateId;
+    }
+
+    public void setUpdateId(Integer updateId) {
+        this.updateId = updateId;
+    }
+
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public String getBackup1() {
+        return backup1;
+    }
+
+    public void setBackup1(String backup1) {
+        this.backup1 = backup1 == null ? null : backup1.trim();
+    }
+
+    public String getBackup2() {
+        return backup2;
+    }
+
+    public void setBackup2(String backup2) {
+        this.backup2 = backup2 == null ? null : backup2.trim();
+    }
+
+    public String getBackup3() {
+        return backup3;
+    }
+
+    public void setBackup3(String backup3) {
+        this.backup3 = backup3 == null ? null : backup3.trim();
+    }
+}
