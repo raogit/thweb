9d45c0e9e806bde2cfb157e300cab44256870061
raowx <18033419212@qq.com>
2016-07-16 11:17:18 +0800

    q权限

---
+++ th-service/src/main/java/com/tianhong/dao/sys/SysRoleMenuMapper.java
@@ -0,0 +1,8 @@
+package com.tianhong.dao.sys;
+
+import com.tianhong.dao.base.BaseMapper;
+import com.tianhong.domain.sys.SysRoleMenu;
+
+public interface SysRoleMenuMapper extends BaseMapper<SysRoleMenu> {
+
+}
