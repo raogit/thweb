7a0d421b26b1c52d5229e5e237559f22c49b3bdf
raowx <18033419212@qq.com>
2016-08-16 15:34:52 +0800

    天虹置业

--- th-service/src/main/java/com/tianhong/dao/content/ContentMapper.java
+++ th-service/src/main/java/com/tianhong/dao/content/ContentMapper.java
@@ -1,9 +1,17 @@
 package com.tianhong.dao.content;
 
+import java.util.List;
+
+import com.tianhong.dao.base.BaseMapper;
 import com.tianhong.domain.content.Content;
 
-public interface ContentMapper {
-    int insert(Content record);
+public interface ContentMapper extends BaseMapper<Content> {
 
-    int insertSelective(Content record);
+	int updateByPrimaryKeyWithBLOBs(Content record);
+
+	Content selectByMenuId(int menuId) throws Exception;
+
+	List<Content> findPage(Content content) throws Exception;
+
+	int getCount(Content content) throws Exception;
 }
