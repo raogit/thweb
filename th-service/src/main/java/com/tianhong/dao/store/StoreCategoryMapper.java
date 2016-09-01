a9e48c0720cc7ecfeb1668f7c8f93d6d53266d78
raowx <18033419212@qq.com>
2016-09-01 09:59:51 +0800

    修改分类表

--- th-service/src/main/java/com/tianhong/dao/store/StoreCategoryMapper.java
+++
@@ -1,8 +0,0 @@
-package com.tianhong.dao.store;
-
-import com.tianhong.dao.base.BaseMapper;
-import com.tianhong.domain.store.StoreCategory;
-
-public interface StoreCategoryMapper extends BaseMapper<StoreCategory> {
-
-}
