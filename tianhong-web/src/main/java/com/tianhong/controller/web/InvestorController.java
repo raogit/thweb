ffa01835240d20e8f477cd42ebeaacb13451f865
sky <sky>
2016-09-25 13:13:09 +0800

    增加投资者

---
+++ tianhong-web/src/main/java/com/tianhong/controller/web/InvestorController.java
@@ -0,0 +1,163 @@
+/**
+ * 
+ */
+package com.tianhong.controller.web;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.tianhong.controller.base.BaseController;
+import com.tianhong.domain.content.Content;
+import com.tianhong.domain.menu.Menu;
+import com.tianhong.domain.picture.Picture;
+import com.tianhong.service.content.ContentService;
+import com.tianhong.service.menu.MenuService;
+import com.tianhong.service.newscenter.NewsCenterService;
+import com.tianhong.service.picture.PictureService;
+
+/**
+ * @author Administrator
+ *
+ */
+@Controller
+@RequestMapping(value = "/web/investor")
+public class InvestorController extends BaseController {
+
+	private static final Log log = LogFactory.getLog(InvestorController.class);
+
+	@Autowired
+	private MenuService menuService;
+	@Autowired
+	private ContentService contentService;
+	@Autowired
+	private PictureService pictureService;
+	@Autowired
+	private NewsCenterService newsCenterService;
+	
+	/**
+	 * 入口
+	 * @param menuId
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(value = "/index")
+	public Object index(@RequestParam("menuId") int menuId, HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		try {
+			model.put("parentMenu", menuService.getByPrimaryKey(menuId));
+			List<Menu> headMenus = menuService.getSubMenus(172, true);
+			model.put("headMenus", headMenus);
+
+			List<Menu> subMenus = menuService.getSubMenus(menuId, true);
+			model.put("subMenus", subMenus);
+			model.put("menu", subMenus.get(0));
+			Content content = contentService.getByMenuId(181);
+			model.put("content", content);
+			List<Picture> pictures = pictureService.findByMenuId(subMenus.get(0).getId());
+			model.put("pictures", pictures);
+
+		} catch (Exception e) {
+			log.error("", e);
+		}
+		return new ModelAndView("/web/investor/inveIndex", model);
+	}
+
+	/**
+	 * 公司治理
+	 * @param menuId
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(value = "/companyManage")
+	public Object companyManage(@RequestParam("menuId") int menuId, HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		try {
+			model.put("parentMenu", menuService.getByPrimaryKey(menuId));
+			List<Menu> headMenus = menuService.getSubMenus(172, true);
+			model.put("headMenus", headMenus);
+
+			List<Menu> subMenus = menuService.getSubMenus(menuId, true);
+			model.put("subMenus", subMenus);
+			model.put("menu", subMenus.get(0));
+			Content content = contentService.getByMenuId(181);
+			model.put("content", content);
+			List<Picture> pictures = pictureService.findByMenuId(subMenus.get(0).getId());
+			model.put("pictures", pictures);
+
+		} catch (Exception e) {
+			log.error("", e);
+		}
+		return new ModelAndView("/web/investor/companyManage", model);
+	}
+	/**
+	 * 股东回报
+	 * @param menuId
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(value = "/profit")
+	public Object profit(@RequestParam("menuId") int menuId, HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		try {
+			model.put("parentMenu", menuService.getByPrimaryKey(menuId));
+			List<Menu> headMenus = menuService.getSubMenus(172, true);
+			model.put("headMenus", headMenus);
+
+			List<Menu> subMenus = menuService.getSubMenus(menuId, true);
+			model.put("subMenus", subMenus);
+			model.put("menu", subMenus.get(0));
+			Content content = contentService.getByMenuId(181);
+			model.put("content", content);
+			List<Picture> pictures = pictureService.findByMenuId(subMenus.get(0).getId());
+			model.put("pictures", pictures);
+
+		} catch (Exception e) {
+			log.error("", e);
+		}
+		return new ModelAndView("/web/investor/profit", model);
+	}
+	
+	/**
+	 * 宣传与保护
+	 * @param menuId
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(value = "/protect")
+	public Object protect(@RequestParam("menuId") int menuId, HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		try {
+			model.put("parentMenu", menuService.getByPrimaryKey(menuId));
+			List<Menu> headMenus = menuService.getSubMenus(172, true);
+			model.put("headMenus", headMenus);
+
+			List<Menu> subMenus = menuService.getSubMenus(menuId, true);
+			model.put("subMenus", subMenus);
+			model.put("menu", subMenus.get(0));
+			Content content = contentService.getByMenuId(181);
+			model.put("content", content);
+			List<Picture> pictures = pictureService.findByMenuId(subMenus.get(0).getId());
+			model.put("pictures", pictures);
+
+		} catch (Exception e) {
+			log.error("", e);
+		}
+		return new ModelAndView("/web/investor/protect", model);
+	}
+}
---
+++ tianhong-web/src/main/webapp/web/investor/inveIndex.jsp
@@ -0,0 +1,87 @@
+<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html >
+<head>
+    <meta charset="UTF-8">
+	<title>天虹集团官网</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<jsp:include page="../base.jsp"></jsp:include>
+	<style type="text/css">
+		html.full, html.full body {
+			width: 100%;
+			height: 100%;
+			overflow: hidden;
+		}
+		
+		.wrapper {
+			width: 100%;
+		}
+	</style>
+    <script type="text/javascript">
+	    function changeFrameHeight(){
+	        var ifm= document.getElementById("hangqing"); 
+	        ifm.height=document.documentElement.clientHeight;
+	        ifm.width=document.documentElement.clientWidth;
+	   }
+	   window.onresize=function(){  
+	         changeFrameHeight();  
+	    
+	   } 
+    </script>
+</head>
+<body>
+<jsp:include page="../head/head.jsp"></jsp:include>
+    <div class="bodyer">
+        <div class="titleImg_content" style="background:url(../images/i_bg1.jpg) no-repeat center; background-size:cover; filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(../images/i_bg1.jpg,sizingMethod='scale');">
+            <div class="titleImg_img"><img src="../images/i_bg2.png"></div>
+        </div>
+        <div class="nav_content">
+            <div class="nav_contentBlock">
+               <div class="nav_aContent">
+                    <a href="../index.html" class="nav_aBlock icon">首页</a>
+	                <a href="index.html" class="nav_aBlock icon">${parentMenu.name }</a>
+	                <a href="index.html" class="nav_aBlock">${menu.name }</a>
+                </div>
+                <div class="nav_ulBlock">
+	            	<c:forEach var="item" items="${subMenus }" begin="0" step="1" varStatus="itemStatus">
+	                	<c:if test="${item.id==menu.id }">
+	                		<a href="${basePath}${item.link }?menuId=${item.id }" class="nav_liBlock current">${item.name }</a>
+	                	</c:if>
+	                	<c:if test="${item.id!=menu.id }">
+	                		<a href="${basePath}${item.link }?menuId=${item.id }" class="nav_liBlock">${item.name }</a>
+	                	</c:if>
+			        </c:forEach>
+	               <!--  <a href="index.html?p=0" class="nav_liBlock">企业文化</a>
+	                <a href="Trailer.html?p=1" class="nav_liBlock">宣传片</a>
+	                <a href="Ejournals.html?p=2" class="nav_liBlock">电子期刊</a>
+	                <a href="Video.html?p=3" class="nav_liBlock">视频中心</a>
+	                <a href="Public.html?p=4" class="nav_liBlock">社会责任</a> -->
+	            </div>
+	            <!-- 
+                <div class="nav_ulBlock">
+                    <a href="inveIndex.html?p=0" class="nav_liBlock">实时行情</a>
+                    <a href="inveInfo.html?p=1" class="nav_liBlock">基本资料</a>
+                    <a href="companyManage.html?p=2" class="nav_liBlock">公司治理</a>
+                    <a href="http://www.cninfo.com.cn" target="_blank" class="nav_liBlock">定期报告</a>
+                    <a href="http://www.cninfo.com.cn" target="_blank" class="nav_liBlock">临时公告</a>
+                    <a href="profit.html?p=5" class="nav_liBlock">股东回报</a>
+                    <a href="protect.html?p=6" class="nav_liBlock">宣传与保护</a>
+                    <a href="contact.html?p=7" class="nav_liBlock">互动交流</a>
+                </div>
+                 -->
+            </div>
+        </div>
+        <div id='hangqingdiv' >
+			<iframe scrolling="no" id="hangqing" name="hangqing" scrolling="no" onload="changeFrameHeight()" frameborder="0" src="http://js.jrj.com.cn/hq/gs/gshq.html?code=002419"   ></iframe>
+		</div>
+		
+	<jsp:include page="../foot/foot.jsp"></jsp:include>
+	<script>
+	    $(function(){
+	        $(".bd-L-cont.LC p").eq(0).css({"width":"420px"});
+	    })
+	</script>
+</body>
+</html>
